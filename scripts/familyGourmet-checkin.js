52643df66422e61cf90d9522ab26897411e60778
Greyson-PC <ssuyu0216@gmail.com>
2022-10-10 01:56:00 +0800

    update

--- scripts/familyGourmet-checkin.js
+++ scripts/familyGourmet-checkin.js
@@ -7,12 +7,12 @@ let account = $persistentStore.read("FamilyGourmetAccount");
 let tokenkey = $persistentStore.read("FamilyGourmetTokenkey");
 
 let getActivityBody = {
-    "mac": mac,
-    "isFrom": "figapp",
-    "Tokenkey": tokenkey,
-    "EnterPriseID": enterPriseID,
-    "Account": account,
-    "act": "getactivity"
+    mac: mac,
+    isFrom: "figapp",
+    Tokenkey: tokenkey,
+    EnterPriseID: enterPriseID,
+    Account: account,
+    act: "getactivity",
 };
 
 let activityID;
@@ -25,74 +25,93 @@ let req = {
 };
 
 req.body = JSON.stringify(getActivityBody);
-$httpClient.post(req, function (error, response, data) {
-    if (error) {
-        $notification.post("🍽 全家餐飲", "尋找活動失敗", error);
-		console.log(`尋找活動失敗1 ${error}`);
-    } else {
-        if (response.status === 200) {
-            try {
-				data = data.replace('"[',"").replace(']"',"").replaceAll("\\","");
-                const obj =  JSON.parse(data);
 
-                if (obj.Remark.ActivityTitle === "每日固定簽到活動") {
-					activityID = obj.Remark.GID;
-					console.log(`找到每日簽到活動 ${activityID}`);
-                } else {
-                    $notification.post("🍽 全家餐飲", "找不到每日簽到活動 ‼️", obj.ErrorMsg);
-					console.log(`找不到每日簽到活動 ${obj}`);
-                }
-            } catch (error) {
+getActivity()
+    .then(() => {
+        checkin();
+    })
+    .catch(() => {});
+$done();
+
+
+function getActivity() {
+    return new Promise((resolve, reject) => {
+        $httpClient.post(req, function (error, response, data) {
+            if (error) {
                 $notification.post("🍽 全家餐飲", "尋找活動失敗", error);
-				console.log(`尋找活動失敗2 ${error}`);
-            }
-        } else {
-            $notification.post("🍽 全家餐飲", "尋找活動失敗", error);
-			console.log(`尋找活動失敗3 ${error}`);
-        }
-    }
-});
+                console.log(`尋找活動失敗1 ${error}`);
+                reject();
+            } else {
+                if (response.status === 200) {
+                    try {
+                        data = data.replace('"[', "").replace(']"', "").replaceAll("\\", "");
+                        const obj = JSON.parse(data);
 
-if(!activityID){
-	$done();
+                        if (obj.Remark.ActivityTitle === "每日固定簽到活動") {
+                            activityID = obj.Remark.GID;
+                            console.log(`找到每日簽到活動 ${activityID}`);
+                            resolve();
+                        } else {
+                            $notification.post("🍽 全家餐飲", "找不到每日簽到活動 ‼️", obj.ErrorMsg);
+                            console.log(`找不到每日簽到活動 ${obj}`);
+                            reject();
+                        }
+                    } catch (error) {
+                        $notification.post("🍽 全家餐飲", "尋找活動失敗", error);
+                        console.log(`尋找活動失敗2 ${error}`);
+                        reject();
+                    }
+                } else {
+                    $notification.post("🍽 全家餐飲", "尋找活動失敗", error);
+                    console.log(`尋找活動失敗3 ${error}`);
+                    reject();
+                }
+            }
+        });
+    });
 }
 
-
-let checkinBody = {
-    isFrom: "figapp",
-    act: "vipactivity",
-    mac: mac,
-    EnterPriseID: enterPriseID,
-    ActivityID: activityID,
-    Account: account,
-    Tokenkey: tokenkey,
-};
-console.log(`checkinBody`);
-console.log(checkinBody);
-
-req.body = JSON.stringify(checkinBody);
-$httpClient.post(req, function (error, response, data) {
-    if (error) {
-        $notification.post("🍽 全家餐飲", "打卡失敗1", error);
-		console.log(`打卡失敗1 ${error}`);
-    } else {
-        if (response.status === 200) {
-            try {
-                const obj = JSON.parse(data);
-                if (obj.ErrorMsg.includes("今日已簽到成功")) {
-                    $notification.post("🍽 全家餐飲", "打卡成功 ✅", obj.ErrorMsg);
-                } else {
-                    $notification.post("🍽 全家餐飲", "打卡失敗2 ‼️", obj.ErrorMsg);
-					console.log(`打卡失敗2 ${obj}`);
-                }
-            } catch (error) {
-                $notification.post("🍽 全家餐飲", "打卡失敗3", error);
-				console.log(`打卡失敗3 ${error}`);
-            }
-        } else {
-            $notification.post("🍽 全家餐飲", "打卡失敗4", error);
-			console.log(`打卡失敗4 ${error}`);
-        }
+function checkin() {
+    if (!activityID) {
+        return;
     }
-    $done();
-});
+
+    let checkinBody = {
+        isFrom: "figapp",
+        act: "vipactivity",
+        mac: mac,
+        EnterPriseID: enterPriseID,
+        ActivityID: activityID,
+        Account: account,
+        Tokenkey: tokenkey,
+    };
+
+    console.log(`checkinBody`);
+    console.log(checkinBody);
+
+    req.body = JSON.stringify(checkinBody);
+    $httpClient.post(req, function (error, response, data) {
+        if (error) {
+            $notification.post("🍽 全家餐飲", "打卡失敗1", error);
+            console.log(`打卡失敗1 ${error}`);
+        } else {
+            if (response.status === 200) {
+                try {
+                    const obj = JSON.parse(data);
+                    if (obj.ErrorMsg.includes("今日已簽到成功")) {
+                        $notification.post("🍽 全家餐飲", "打卡成功 ✅", obj.ErrorMsg);
+                    } else {
+                        $notification.post("🍽 全家餐飲", "打卡失敗2 ‼️", obj.ErrorMsg);
+                        console.log(`打卡失敗2 ${obj}`);
+                    }
+                } catch (error) {
+                    $notification.post("🍽 全家餐飲", "打卡失敗3", error);
+                    console.log(`打卡失敗3 ${error}`);
+                }
+            } else {
+                $notification.post("🍽 全家餐飲", "打卡失敗4", error);
+                console.log(`打卡失敗4 ${error}`);
+            }
+        }
+    });
+}
