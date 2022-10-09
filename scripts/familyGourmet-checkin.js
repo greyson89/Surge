3e38ec8b4a5b201424f189e87cd1267a86eabbe2
Greyson-PC <ssuyu0216@gmail.com>
2022-10-09 16:48:46 +0800

    自動打卡測試

--- scripts/familyGourmet-checkin.js
+++ scripts/familyGourmet-checkin.js
@@ -1,4 +1,45 @@
 // 每日打卡
 // Ver.TEST
+let body = {
+    isFrom: "figapp",
+    act: "vipactivity",
+    mac: $persistentStore.read("FamilyGourmetMac"),
+    EnterPriseID: $persistentStore.read("FamilyGourmetEnterPriseID"),
+    ActivityID: $persistentStore.read("FamilyGourmetActivityID"),
+    Account: $persistentStore.read("FamilyGourmetAccount"),
+    Tokenkey: $persistentStore.read("FamilyGourmetTokenKey"),
+};
 
-$done();
+let checkinRequest = {
+    url: "https://fig.jh8.tw/public/AppDataVIP.ashx",
+    headers: {
+        "content-type": "application/application/x-www-form-urlencoded",
+    },
+    body: JSON.stringify(body),
+};
+
+$httpClient.post(checkinRequest, function (error, response, data) {
+    if (error) {
+        $notification.post("🍽 全家餐飲", "打卡失敗1", error);
+		console.log(`打卡失敗1 ${error}`);
+    } else {
+        if (response.status === 200) {
+            try {
+                const obj = JSON.parse(data);
+                if (obj.ErrorMsg.incudes("今日已簽到成功")) {
+                    $notification.post("🍽 全家餐飲", "打卡成功 ✅", obj.ErrorMsg);
+                } else {
+                    $notification.post("🍽 全家餐飲", "打卡失敗2 ‼️", obj.ErrorMsg);
+					console.log(`打卡失敗2 ${obj.ErrorMsg}`);
+                }
+            } catch (error) {
+                $notification.post("🍽 全家餐飲", "打卡失敗3", error);
+				console.log(`打卡失敗3 ${error}`);
+            }
+        } else {
+            $notification.post("🍽 全家餐飲", "打卡失敗4", error);
+			console.log(`打卡失敗4 ${error}`);
+        }
+    }
+    $done();
+});
--- scripts/familyGourmet-get-token.js
+++ scripts/familyGourmet-get-token.js
@@ -1,42 +1,14 @@
 // 嘗試取得token
 
 let body = JSON.parse($request.body);
-console.log(`$request.body = ${typeof $request.body}`);
-console.log(`body = ${typeof $request.body}`);
-console.log(body);
 
-let result = '';
-if(body.mac){
-	result += '取得mac\n';
-	console.log(`get mac ${body.mac}`);
+if(body.mac && body.EnterPriseID && body.ActivityID && body.Account && body.TokenKey){
 	$persistentStore.write(body.mac,'FamilyGourmetMac');
-}else{
-	result += '未取得mac\n';
-	console.log(`get mac ${body.mac}`);
-}
-if(body.EnterPriseID){
-	result += '取得enterPriseID\n';
-	console.log(`get mac ${body.EnterPriseID}`);
 	$persistentStore.write(body.EnterPriseID,'FamilyGourmetEnterPriseID');
-}
-if(body.ActivityID){
-	result += '取得ActivityID\n';
-	console.log(`get mac ${body.ActivityID}`);
 	$persistentStore.write(body.ActivityID,'FamilyGourmetActivityID');
-}
-if(body.Account){
-	result += '取得Account\n';
-	console.log(`get mac ${body.Account}`);
 	$persistentStore.write(body.Account,'FamilyGourmetAccount');
-}
-if(body.TokenKey){
-	result += '取得TokenKey\n';
-	console.log(`get mac ${body.TokenKey}`);
 	$persistentStore.write(body.TokenKey,'FamilyGourmetTokenKey');
+	$notification.post('🍽 全家餐飲','已更新必要Token','');
 }
 
-
-
-$notification.post('🍽 全家餐飲',result,'');
-
-$done({body:body});
+$done({body:$request.body});
