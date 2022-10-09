18d32a90a1ce23dee2f9e9bd39cabe93d0fa26ac
Greyson-PC <ssuyu0216@gmail.com>
2022-10-09 21:02:55 +0800

    調整錯誤名稱

--- scripts/familyGourmet-get-token.js
+++ scripts/familyGourmet-get-token.js
@@ -5,13 +5,13 @@ let body = JSON.parse($request.body);
 if(body.mac && body.EnterPriseID && body.Account && body.TokenKey){
 	$persistentStore.write(body.mac,'FamilyGourmetMac');
 	$persistentStore.write(body.EnterPriseID,'FamilyGourmetEnterPriseID');
-	// $persistentStore.write(body.ActivityID,'FamilyGourmetActivityID');
 	$persistentStore.write(body.Account,'FamilyGourmetAccount');
-	$persistentStore.write(body.TokenKey,'FamilyGourmetTokenKey');
+	$persistentStore.write(body.Tokenkey,'FamilyGourmetTokenkey');
 	$notification.post('🍽 全家餐飲','已更新必要Token','');
-	console.log(`全家 get token success ${body}`)
+	console.log(`全家 get token success`);
 }else{
-	console.log(`全家 get token error ${body}`)
+	console.log(`全家 get token error`);
+	console.log(body);
 }
 
 $done({body:$request.body});
