9c04b085b6a9a914b7b11ff28b97b98b91e427e7
Greyson-PC <ssuyu0216@gmail.com>
2022-10-09 15:00:41 +0800

    調整分支連結

--- scripts/familyGourmet-get-token.js
+++ scripts/familyGourmet-get-token.js
@@ -5,19 +5,26 @@ let body = $request.body;
 let result = '';
 if(body.mac){
 	result += '取得mac\n';
+	$persistStore.write(body.mac,'FamilyGourmetMac');
 }
 if(body.EnterPriseID){
 	result += '取得enterPriseID\n';
+	$persistStore.write(body.mac,'FamilyGourmetEnterPriseID');
 }
 if(body.ActivityID){
 	result += '取得ActivityID\n';
+	$persistStore.write(body.mac,'FamilyGourmetActivityID');
 }
 if(body.Account){
 	result += '取得Account\n';
+	$persistStore.write(body.mac,'FamilyGourmetAccount');
 }
 if(body.TokenKey){
 	result += '取得TokenKey\n';
+	$persistStore.write(body.mac,'FamilyGourmetTokenKey');
 }
+
+
 
 $notification.post('🍽 全家餐飲',result,'');
 
