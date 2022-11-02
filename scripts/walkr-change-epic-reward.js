bbfb65314b09f4a3cea0a41d65aea85d70602c2b
Greyson-PC <ssuyu0216@gmail.com>
2022-11-02 21:02:17 +0800

    測試修改EPIC獎勵

---
+++ scripts/walkr-change-epic-reward.js
@@ -0,0 +1,18 @@
+// Walkr change the cube to replicator and increase coins  UAT
+// Walkr 更改取得的能量方塊為設計圖 金幣提高
+// execute when login account
+
+let body = JSON.parse($response.body);
+
+
+body.fetchable_contents[0].key="replicator";
+body.fetchable_contents[0].value="map";
+
+let vSplit = body.fetchable_contents[1].value.split(".");
+let newValue =  Number(vSplit)+20_000_000;
+body.fetchable_contents[1].value =+vSplit[1]+vSplit[2];
+
+console.log(`🚀 獎勵調整成功 能量方塊變更為DFR設計圖\n金幣增加為${newValue}`);
+$notification.post('🚀 Walkr',`獎勵調整成功 能量方塊變更為DFR設計圖\n金幣增加為${newValue}`,'');
+
+$done({ body: JSON.stringify(body) });
