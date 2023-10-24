660985c0bc3e9f3c59d22e1b2ae54d6695a621fe
Greyson <ssuyu0216@gmail.com>
2023-10-25 02:09:07 +0800

    調整可能因取得暫存而發生錯誤

--- scripts/walkr-bridge-energy.js
+++ scripts/walkr-bridge-energy.js
@@ -1,13 +1,20 @@
 // Walkr bridge co-pilots supply energy data modify
 // Walkr 修改每次進入時艦橋共駕提供的能量數據 需搭配另一腳本才能正確取得
 
-let body = JSON.parse($response.body);
 
-for (const pilot of body.pilots) {
-	pilot.energy =160000;
+if($response.body == undefined){
+	$done({});
+}else{
+
+	let body = JSON.parse($response.body);
+	
+	for (const pilot of body.pilots) {
+		pilot.energy =160000;
+	}
+	
+	console.log(`🚀 修改共駕能量數據完成`);
+	//$notification.post('🚀 Walkr','修改共駕能量數據','');
+	
+	$done({body:JSON.stringify(body)});
 }
 
-console.log(`🚀 修改共駕能量數據完成`);
-//$notification.post('🚀 Walkr','修改共駕能量數據','');
-
-$done({body:JSON.stringify(body)});
--- scripts/walkr-change-epic-reward.js
+++ scripts/walkr-change-epic-reward.js
@@ -2,34 +2,38 @@
 // Walkr 更改取得傳說獎勵，可選擇能量方塊或設計圖，金幣提高
 // 可自行調整選擇
 
-let body = JSON.parse($response.body);
-let select = "cubes"; // 輸入cubes或是map切換
+if ($response.body == undefined) {
+    $done({});
+} else {
+    let body = JSON.parse($response.body);
+    let select = "cubes"; // 輸入cubes或是map切換
 
-let isTarget = true;
-if (body.fetchable_contents == undefined) {
-    isTarget = false;
-} else if (body.fetchable_contents.length == 0) {
-    isTarget = false;
-}
-
-if (isTarget) {
-    let vSplit = body.fetchable_contents[1].value.split(".");
-    let newValue = Number(vSplit[0]) + 200000000;
-    body.fetchable_contents[1].value = `${newValue}.${vSplit[1]}.${vSplit[2]}`;
-
-    if (select == "cubes") {
-        let newCubes = 400000;
-        body.fetchable_contents[0].value = newCubes;
-
-        console.log(`🚀 獎勵調整成功 能量方塊增加為${newCubes}\n金幣增加為${newValue}`);
-        $notification.post("🚀 Walkr", `獎勵調整成功 能量方塊增加為${newCubes}\n金幣增加為${newValue}`);
-    } else if (select == "map") {
-        body.fetchable_contents[0].key = "replicator";
-        body.fetchable_contents[0].value = "map";
-
-        console.log(`🚀 獎勵調整成功 能量方塊變更為DFR設計圖\n金幣增加為${newValue}`);
-        $notification.post("🚀 Walkr", `獎勵調整成功 能量方塊變更為DFR設計圖\n金幣增加為${newValue}`, "");
+    let isTarget = true;
+    if (body.fetchable_contents == undefined) {
+        isTarget = false;
+    } else if (body.fetchable_contents.length == 0) {
+        isTarget = false;
     }
-}
 
-$done({ body: JSON.stringify(body) });
+    if (isTarget) {
+        let vSplit = body.fetchable_contents[1].value.split(".");
+        let newValue = Number(vSplit[0]) + 200000000;
+        body.fetchable_contents[1].value = `${newValue}.${vSplit[1]}.${vSplit[2]}`;
+
+        if (select == "cubes") {
+            let newCubes = 400000;
+            body.fetchable_contents[0].value = newCubes;
+
+            console.log(`🚀 獎勵調整成功 能量方塊增加為${newCubes}\n金幣增加為${newValue}`);
+            $notification.post("🚀 Walkr", `獎勵調整成功 能量方塊增加為${newCubes}\n金幣增加為${newValue}`);
+        } else if (select == "map") {
+            body.fetchable_contents[0].key = "replicator";
+            body.fetchable_contents[0].value = "map";
+
+            console.log(`🚀 獎勵調整成功 能量方塊變更為DFR設計圖\n金幣增加為${newValue}`);
+            $notification.post("🚀 Walkr", `獎勵調整成功 能量方塊變更為DFR設計圖\n金幣增加為${newValue}`, "");
+        }
+    }
+
+    $done({ body: JSON.stringify(body) });
+}
